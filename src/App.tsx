9dc198e6cd34b2b3832121456f2a67bcbe88afb1
Karanveer Masoun <143984706+ksmasoun@users.noreply.github.com>
2025-05-21 13:58:47 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+import React, { useState } from 'react';
+function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-2xl font-bold text-blue-600">Tailwind + TypeScript Working!</h1>
+    </div>
+  );
+}
+
+export default App;
