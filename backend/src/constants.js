09699d0e08bccb97545e0f1aecf10f21bc289bdd
Somya Dwivedi <somya.dwivedi@yahoo.com>
2025-05-26 18:08:33 -0400

    refactor: Clean up constants and remove unused question categories

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,22 +1,21 @@
 export const DB_NAME = "database_Test";
- 
+
 // Enums for question Type
 export const QUESTION_TYPE = Object.freeze({
   MCQ: "Mcq",
   INPUT: "Input",
 });
- 
+
 // Enums for question Category
 export const QUESTION_CATEGORY = Object.freeze({
   VOCABULARY: "Vocabulary",
-  LITERATUREandCULTURE: "Literature",
-  GRAMMARandLANGUAGE: "Grammar",
+  CULTURE: "Culture",
+  GRAMMAR: "Grammar",
 });
- 
+
 // Enums for question Level
 export const QUESTION_LEVEL = Object.freeze({
   BEGINNER: "Beginner",
   INTERMEDIATE: "Intermediate",
   ADVANCED: "Advanced",
 });
- 
