5e9c658fcb69429a534ce0b4d7613cc65b1ef0e5
Somya Dwivedi <somya.dwivedi@yahoo.com>
2025-05-29 03:57:55 -0400

    fix: Restore checkApiKey middleware for answer and question routes

--- backend/src/routes/question.routes.js
+++ backend/src/routes/question.routes.js
@@ -5,18 +5,18 @@ import {
   getQuestion,
   updateQuestionById,
 } from "../controllers/question.controller.js";
-// import { checkApiKey } from "../middlewares/apiKey.js";
+import { checkApiKey } from "../middlewares/apiKey.js";
 
 const questionRouter = Router();
 
-// questionRouter.route("/surveyQuestions").post(checkApiKey, addQuestions);
-// questionRouter.route("/").put(checkApiKey, updateQuestionById);
-// questionRouter.route("/").delete(checkApiKey, deleteQuestionById);
-// questionRouter.route("/").get(checkApiKey, getQuestion);
+questionRouter.route("/surveyQuestions").post(checkApiKey, addQuestions);
+questionRouter.route("/").put(checkApiKey, updateQuestionById);
+questionRouter.route("/").delete(checkApiKey, deleteQuestionById);
+questionRouter.route("/").get(checkApiKey, getQuestion);
 
-questionRouter.route("/surveyQuestions").post(addQuestions);
-questionRouter.route("/").put(updateQuestionById);
-questionRouter.route("/").delete(deleteQuestionById);
-questionRouter.route("/").get(getQuestion);
+// questionRouter.route("/surveyQuestions").post(addQuestions);
+// questionRouter.route("/").put(updateQuestionById);
+// questionRouter.route("/").delete(deleteQuestionById);
+// questionRouter.route("/").get(getQuestion);
 
 export default questionRouter;
