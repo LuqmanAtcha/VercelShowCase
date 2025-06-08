8d9e072c2868d735a3882e1cf4a8285241790c3d
Somya Dwivedi <somya.dwivedi@yahoo.com>
2025-06-08 05:31:16 -0400

    refactor: Update import path for Question type in useAnalyticsData hook for improved code organization

--- src/components/hooks/useAnalyticsData.ts
+++ src/components/hooks/useAnalyticsData.ts
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { Question } from "../../types";
+import { Question } from "../../types/types";
 
 interface AnalyticsData {
   categoryCounts: Record<string, number>;
